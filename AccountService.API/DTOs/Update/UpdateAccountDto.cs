6fa026f049a37956bc69bde7cd047432b4c661b4
RubenFern <rubenferval@hotmail.com>
2025-09-21 12:56:08 +0200

    remove property from dto

--- AccountService.API/DTOs/Update/UpdateAccountDto.cs
+++ AccountService.API/DTOs/Update/UpdateAccountDto.cs
@@ -5,7 +5,6 @@ namespace AccountService.API.DTOs.Update;
 public record UpdateAccountDto : UpdateBaseDto
 {
     public Guid? GenderCode { get; set; }
-    public string? Username { get; set; }
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public Guid? CountryCode { get; set; }
--- AccountService.Application/DTOs/Update/UpdateAccountEntityDto.cs
+++ AccountService.Application/DTOs/Update/UpdateAccountEntityDto.cs
@@ -5,7 +5,6 @@ namespace AccountService.Application.DTOs.Update;
 public record UpdateAccountEntityDto
 {
     public Guid? GenderCode { get; set; }
-    public string? Username { get; set; }
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public Guid? CountryCode { get; set; }
